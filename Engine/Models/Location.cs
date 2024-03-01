5a23f41ca8465a8276ff5f18be447380fb1a5318
Radge Carbonel <rac4843@students.uc-bcf.edu.ph>
2024-03-01 19:34:55 +0800

    ror manen

--- Engine/Models/Location.cs
+++ Engine/Models/Location.cs
@@ -12,7 +12,7 @@ namespace Engine.Models
         public int YCoordinate { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public string ImagePath { get; set; }
+        public string ImageName { get; set; }
 
     }
 }
--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -26,7 +26,7 @@ namespace Engine.ViewModels
             CurrentLocation.YCoordinate = 0;
             CurrentLocation.Name = "Home";
             CurrentLocation.Description = "This is your house";
-            CurrentLocation.ImagePath = "pack://application:,,,/Engine;component/Images/Locations/home.jpg";
+            CurrentLocation.ImageName = "pack://application:,,,/Engine;component/Images/Locations/home.jpg";
         }
     }
 }
