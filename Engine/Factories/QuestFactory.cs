7dba415fedad0283ffbb2ef04a991f6f57d3da39
Yasmien Gayao <ycg1531@students.uc-bcf.edu.ph>
2024-03-21 21:33:08 +0800

    YESDFGHSFHAS

--- Engine/Factories/QuestFactory.cs
+++ Engine/Factories/QuestFactory.cs
@@ -19,13 +19,57 @@ namespace Engine.Factories
             rewardItems.Add(new ItemQuantity(1002, 1));
 
             _quests.Add(new Quest(1,
-                                  "Clear the herb garden",
-                                  "Defeat the snakes in the Herbalist's garden",
+                                  "The Missing Letter: Look for the missing letter of Choco Pirate Cookie",
+                                  "It is said that on the Choco Pirate's journey across the Cinnamon Coral Ocean, he lost his treasure and left a letter there.",
                                   itemsToComplete,
                                   25, 10,
                                   rewardItems));
+            _quests.Add(new Quest(2,
+                                  "Oh no, we can't cross: Make a Bridge to Bamboo Cookie Forest",
+                                  "Time to play architect and build a bridge that’s strong enough for a cookie, but not too tasty for them to eat!"
+                                  "The bridge connecting Bamboo Cookie Forest and Fondue Lake was said to be destroyed by a group of Burnt Cookies due to an argument about who was the crumbiest amongst them",
+                                  itemsToComplete,
+                                  25, 10,
+                                  rewardItems));
+            _quests.Add(new Quest(3,
+                                  "Matcha Tea Time Alliance: Befriend Mayor Green Tea Butter",
+                                  "Win over Mayor Green Tea Butter for him to give a special ingredient"
+                                  "Let me tell you a rumor, there's a mayor in Matcha Green Province that likes butter with his tea. Yikes! don't let the breadish people know!",
+                                  itemsToComplete,
+                                  25, 10,
+                                  rewardItems));
+            _quests.Add(new Quest(4,
+                                "Crumby Encounter: Look for Macaroon Cookie and give him the ingredients for “Golden Croissant”",
+                                "Seek out Macaroon Cookie at the Bamboo Cookie Forest, the master baker with a golden touch for croissants. Rumor has it, his pastries are so shiny, you might just need sunglasses to see them!",
+                                itemsToComplete,
+                                25, 10,
+                                rewardItems));
+            _quests.Add(new Quest(5,
+                                "",
+                                "",
+                                itemsToComplete,
+                                25, 10,
+                                rewardItems));
+            _quests.Add(new Quest(6,
+                                "",
+                                "",
+                                itemsToComplete,
+                                25, 10,
+                                rewardItems));
+            _quests.Add(new Quest(7,
+                                "",
+                                "",
+                                itemsToComplete,
+                                25, 10,
+                                rewardItems));
+            _quests.Add(new Quest(8,
+                                "",
+                                "",
+                                itemsToComplete,
+                                25, 10,
+                                rewardItems));
         }
-        internal static Quest GetQuestByID(int id)
+            internal static Quest GetQuestByID(int id)
         {
             return _quests.FirstOrDefault(quest => quest.ID == id);
         }
