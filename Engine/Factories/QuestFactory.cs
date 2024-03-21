8e10ce6102488a16174ecd33f8b2fc86ed235a55
Gerryle R. Martinez <grm7867@students.uc-bcf.edu.ph>
2024-03-21 19:43:01 -0400

    187326

--- Engine/Factories/QuestFactory.cs
+++ Engine/Factories/QuestFactory.cs
@@ -16,32 +16,44 @@ namespace Engine.Factories
             List<ItemQuantity> itemsToComplete = new List<ItemQuantity>();
             List<ItemQuantity> rewardItems = new List<ItemQuantity>();
             itemsToComplete.Add(new ItemQuantity(9001, 5));
-            rewardItems.Add(new ItemQuantity(1002, 1));
+            itemsToComplete.Add(new ItemQuantity(9002, 5));
+            itemsToComplete.Add(new ItemQuantity(9003, 5));
+            itemsToComplete.Add(new ItemQuantity(9004, 5));
+            itemsToComplete.Add(new ItemQuantity(9005, 5));
+            itemsToComplete.Add(new ItemQuantity(9006, 5));
+            itemsToComplete.Add(new ItemQuantity(9007, 5));
+            rewardItems.Add(new ItemQuantity(64, 1000));
+            rewardItems.Add(new ItemQuantity(65, 1));
+            rewardItems.Add(new ItemQuantity(66, 1));
+            rewardItems.Add(new ItemQuantity(67, 1));
+            rewardItems.Add(new ItemQuantity(68, 1));
+            rewardItems.Add(new ItemQuantity(69, 1));
+            rewardItems.Add(new ItemQuantity(70, 1));
 
             _quests.Add(new Quest(1,
                                   "The Missing Letter: Look for the missing letter of Choco Pirate Cookie",
                                   "It is said that on the Choco Pirate's journey across the Cinnamon Coral Ocean, he lost his treasure and left a letter there.",
-                                  itemsToComplete,
+                                  itemsToComplete[1],
                                   25, 10,
                                   rewardItems));
             _quests.Add(new Quest(2,
                                   "Oh no, we can't cross: Make a Bridge to Bamboo Cookie Forest",
-                                  "Time to play architect and build a bridge that’s strong enough for a cookie, but not too tasty for them to eat!"
-                                  "The bridge connecting Bamboo Cookie Forest and Fondue Lake was said to be destroyed by a group of Burnt Cookies due to an argument about who was the crumbiest amongst them",
-                                  itemsToComplete,
+                                  "Time to play architect and build a bridge that’s strong enough for a cookie, but not too tasty for them to eat!" +
+                                  "\nThe bridge connecting Bamboo Cookie Forest and Fondue Lake was said to be destroyed by a group of Burnt Cookies due to an argument about who was the crumbiest amongst them",
+                                  itemsToComplete[0],
                                   25, 10,
                                   rewardItems));
             _quests.Add(new Quest(3,
                                   "Matcha Tea Time Alliance: Befriend Mayor Green Tea Butter",
-                                  "Win over Mayor Green Tea Butter for him to give a special ingredient"
-                                  "Let me tell you a rumor, there's a mayor in Matcha Green Province that likes butter with his tea. Yikes! don't let the breadish people know!",
-                                  itemsToComplete,
+                                  "Win over Mayor Green Tea Butter for him to give a special ingredient" +
+                                  "\nLet me tell you a rumor, there's a mayor in Matcha Green Province that likes butter with his tea. Yikes! don't let the breadish people know!",
+                                  itemsToComplete[3],
                                   25, 10,
                                   rewardItems));
             _quests.Add(new Quest(4,
                                 "Crumby Encounter: Look for Macaroon Cookie and give him the ingredients for “Golden Croissant”",
                                 "Seek out Macaroon Cookie at the Bamboo Cookie Forest, the master baker with a golden touch for croissants. Rumor has it, his pastries are so shiny, you might just need sunglasses to see them!",
-                                itemsToComplete,
+                                itemsToComplete[4],
                                 25, 10,
                                 rewardItems));
             _quests.Add(new Quest(5,
@@ -53,13 +65,13 @@ namespace Engine.Factories
             _quests.Add(new Quest(6,
                                 "Land of Cleanliness: Get White Chocolate Mocha Concentrate",
                                 "There was once a legend that hides on white waters, they lived in concentrated that fall naturally from the mountain at White Chocolate Mocha Falls.",
-                                itemsToComplete,
+                                itemsToComplete[5],
                                 25, 10,
                                 rewardItems));
             _quests.Add(new Quest(7,
                                 "Let the path lead you: Follow Golden Prophet Cookie to the City",
                                 "When you surrender to the divine, you let them lead the path for you. The residents think it’s cheesy, but even on the most Tropical Cheesy Beach, you will find your meaning.",
-                                itemsToComplete,
+                                itemsToComplete[6],
                                 25, 10,
                                 rewardItems));
             _quests.Add(new Quest(8,
--- Engine/Models/Quest.cs
+++ Engine/Models/Quest.cs
@@ -8,6 +8,13 @@ namespace Engine.Models
 {
     public class Quest
     {
+        private int v1;
+        private string v2;
+        private string v3;
+        private ItemQuantity itemQuantity;
+        private int v4;
+        private int v5;
+
         public int ID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
@@ -23,6 +30,17 @@ namespace Engine.Models
             ItemsToComplete = itemsToComplete;
             RewardExpPoints = rewardExpPoints;
             RewardGold = rewardGold;
+            RewardItems = rewardItems;
+        }
+
+        public Quest(int v1, string v2, string v3, ItemQuantity itemQuantity, int v4, int v5, List<ItemQuantity> rewardItems)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+            this.itemQuantity = itemQuantity;
+            this.v4 = v4;
+            this.v5 = v5;
             RewardItems = rewardItems;
         }
     }
