7b6ea7ed8c13a29cdaaf8505a8ccbc73a81da98f
LUCCI ANIA LUISSE FAGELA <ljf7925@students.uc-bcf.edu.ph>
2024-03-21 20:17:54 +0800

    and when im back in chicago, i feel it

--- Engine/Factories/ItemFactory.cs
+++ Engine/Factories/ItemFactory.cs
@@ -13,8 +13,16 @@ namespace Engine.Factories
         static ItemFactory()
         {
             _standardGameItems = new List<GameItem>();
-            _standardGameItems.Add(new Weapon(64, "Cheese Stick", 1,"", 1, 3));
-            _standardGameItems.Add(new Weapon(65, "Candy Dagger", 10,"", 2, 7));
+            _standardGameItems.Add(new GameItem(64, "Bear Jelly Coins", 1,""));
+            _standardGameItems.Add(new GameItem(65, "Star Jelly", 10,""));
+            _standardGameItems.Add(new GameItem(66, "Cookie Powder", 10, ""));
+            _standardGameItems.Add(new GameItem(67, "Sugar Crystals", 10, ""));
+            _standardGameItems.Add(new GameItem(68, "Insignia Cookie Treasures", 10, ""));
+            _standardGameItems.Add(new GameItem(69, "Crystal Jam", 10, ""));
+            _standardGameItems.Add(new GameItem(70, "Golden Croissant", 10, ""));
+            _standardGameItems.Add(new Weapon(71, "Cookie Axe", 10, "", 2, 7));
+            _standardGameItems.Add(new Weapon(72, "Cookie Gun", 10, "", 2, 7));
+
         }
         public static GameItem CreateGameItem(int itemTypeID)
         {
